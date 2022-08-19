ca747e8043f770729c2e4447f91de51f7663844e
ronitmagar <rvmagar2000@gmail.com>
2022-08-19 14:09:08 +0530

    added physicsMechTopics

--- skool/alpha/views.py
+++ skool/alpha/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+import os
+from pathlib import Path
+from alpha import subjectSections
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'base.html')
+    
+
+def Physics(request):
+    content = {
+        'sub_name' : 'Physics',
+        'list_sections' : subjectSections.physicsSection,
+    }
+    return render(request,'physics.html', content)
+
+def Maths(request):
+    content = {
+        'sub_name' : 'Maths',
+        'list_sections' : subjectSections.mathsSection,
+    }
+    return render(request,'maths.html', content)
+
+def Chemistry(request):
+    content = {
+        'sub_name' : 'Chemistry',
+        'list_sections' : subjectSections.chemistrySection,
+    }
+    return render(request,'chemistry.html', content)
+
+def Sections(request):
+    return render(request, 'chemistry.html')
+
+
+
+
--- skool/skool/settings.py
+++ skool/skool/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'skool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
